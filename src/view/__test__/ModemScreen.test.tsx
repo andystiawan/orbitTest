8c3e96a086d34310206b09d870be86b4540895e5
andystiawan <andysetiawan310896@gmail.com>
2023-10-26 17:31:06 +0700

    test modemscreen

--- src/view/__test__/ModemScreen.test.tsx
+++ src/view/__test__/ModemScreen.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import { useIsFocused as mockUseIsFocused } from '@react-navigation/native';
 
 import { getModemList } from '../../network/service/serviceModem';
 
 import ModemScreen from '../ModemScreen';
+import ListDevice from '../../component/ListDevice';
 
 jest.mock('../../network/service/serviceModem', () => ({
     getModemList: jest.fn()
@@ -17,8 +18,56 @@ jest.mock('@react-navigation/native', () => ({
 }));
 
 describe('ModemScreen', () => {
+
+    const data = [
+        {
+            id: 11,
+            name: "Mel's Orbit",
+            quota: {
+                currentUsage: 30000,
+                maxUsage: 80000,
+                unit: "MB"
+            },
+            validity: {
+                value: 30,
+                unit: "Days"
+            }
+        },
+        {
+            id: 23,
+            name: "Kitchen Modem",
+            quota: {
+                currentUsage: 55000,
+                maxUsage: 60000,
+                unit: "MB"
+            },
+            validity: {
+                value: 7,
+                unit: "Days"
+            }
+        },
+        {
+            id: 27,
+            name: "Donski Pro",
+            quota: {
+                currentUsage: 25000,
+                maxUsage: 45000,
+                unit: "MB"
+            },
+            validity: {
+                value: 30,
+                unit: "Days"
+            }
+        },
+    ]
+
     beforeEach(() => {
         (mockUseIsFocused as jest.Mock).mockReturnValue(true);
+    });
+
+    it('renders correctly', () => {
+        const { toJSON } = render(<ModemScreen />);
+        expect(toJSON()).toMatchSnapshot();
     });
 
     afterEach(() => {
@@ -33,6 +82,18 @@ describe('ModemScreen', () => {
         expect(getModemList).toHaveBeenCalledTimes(1);
         expect(mockUseIsFocused).toHaveBeenCalledTimes(2);
         expect(mockUseIsFocused).toHaveBeenCalledWith();
+    });
+
+
+    it('should render the ListDevice component with the provided data', () => {
+        const onChangeSort = jest.fn()
+        const reset = jest.fn()
+
+        const { getByTestId } = render(<ListDevice data={data} onChangeSort={onChangeSort} reset={reset} />)
+
+        const listDevice = getByTestId('list-device')
+
+        expect(listDevice).toBeTruthy();
     });
 
 
